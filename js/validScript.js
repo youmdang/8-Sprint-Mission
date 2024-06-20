2114b27950a3101a66158ef6a11c3c05809049a6
yamdang <youm4000@naver.com>
2024-06-20 15:21:45 +0900

    선택자를 id에서 class로 수정, 버튼 비활성화 공통 부분을 함수로 따로 만들어서 적용

--- js/validScript.js
+++ js/validScript.js
@@ -1,13 +1,16 @@
-
-
-const userEmail = document.querySelector("#user-email");
-const userName = document.querySelector("#user-name");
-const userPassword = document.querySelector("#user-password");
-const userPasswordChk = document.querySelector("#user-password-chk");
-const passwordBtn = document.querySelector("#password-btn");
-const passwordBtnChk = document.querySelector("#password-btn-chk");
+const userEmail = document.querySelector(".user-email");
+const userName = document.querySelector(".user-name");
+const userPassword = document.querySelector(".user-password");
+const userPasswordChk = document.querySelector(".user-password-chk");
+const passwordBtn = document.querySelector(".password-btn");
+const passwordBtnChk = document.querySelector(".password-btn-chk");
 const submitBtn = document.querySelector(".submit-btn");
 submitBtn.disabled = true; // 회원가입 버튼 비활성화
+
+function btnFail() {
+	submitBtn.disabled = true; // 회원가입&로그인 버튼 비활성화
+	submitBtn.classList.remove("on"); // 회원가입&로그인 버튼 활성화 css 삭제
+}
 
 userEmail.addEventListener("focusout", (e) => { // 이메일 input에서 focusout 될 경우 실행
 	const userEmailVal = e.target.value.split(" ").join(""); // split(" ").join("")으로 value 띄어쓰기 삭제
@@ -17,14 +20,12 @@ userEmail.addEventListener("focusout", (e) => { // 이메일 input에서 focusou
 		e.target.className = "fail"; // input에 빨간선 추가
 		e.target.nextElementSibling.textContent = "이메일을 입력해주세요."; // input 하단에 경고 문자 추가
 		e.target.nextElementSibling.classList.add("on"); // input 하단 경고 문자 영역 노출
-		submitBtn.disabled = true; // 회원가입 버튼 비활성화
-		submitBtn.classList.remove("on");
+		btnFail()
 	} else if (!emailTest.test(userEmailVal)) { // 이메일 값이 공백은 아니지만 이메일 형식에 맞지 않을 때
 		e.target.className = "fail";
 		e.target.nextElementSibling.textContent = "잘못된 이메일 형식입니다.";
 		e.target.nextElementSibling.classList.add("on");
-		submitBtn.disabled = true;
-		submitBtn.classList.remove("on");
+		btnFail()
 	} else { // 이메일 정상적으로 입력할 때
 		e.target.className = "good"; // input 빨간선 삭제
 		e.target.nextElementSibling.textContent = ""; // input 하단 경고 문자 삭제
@@ -41,8 +42,7 @@ if(userName) {
 			e.target.className = "fail";
 			e.target.nextElementSibling.textContent = "닉네임을 입력해주세요.";
 			e.target.nextElementSibling.classList.add("on");
-			submitBtn.disabled = true;
-			submitBtn.classList.remove("on");
+			btnFail()
 		} else { // 닉네일을 정상적으로 입력했을 때
 			e.target.className = "good";
 			e.target.nextElementSibling.textContent = "";
@@ -59,14 +59,12 @@ userPassword.addEventListener("focusout", (e) => { // 비밀번호 input에서 f
 		e.target.className = "fail";
 		e.target.nextElementSibling.textContent = "비밀번호 입력해주세요.";
 		e.target.nextElementSibling.classList.add("on");
-		submitBtn.disabled = true;
-		submitBtn.classList.remove("on");
+		btnFail()
 	} else if(userPasswordVal.length < 8) { // 비밀번호가 8자리 미만일 때
 		e.target.className = "fail";
 		e.target.nextElementSibling.textContent = "비밀번호 8자 이상 입력해주세요.";
 		e.target.nextElementSibling.classList.add("on");
-		submitBtn.disabled = true;
-		submitBtn.classList.remove("on");
+		btnFail()
 	} else { // 비밀번호를 정상적으로 입력할 때
 		e.target.className = "good";
 		e.target.nextElementSibling.textContent = "";
@@ -83,14 +81,12 @@ if(userPasswordChk) {
 			e.target.className = "fail";
 			e.target.nextElementSibling.textContent = "비밀번호 일치하지 않습니다.";
 			e.target.nextElementSibling.classList.add("on");
-			submitBtn.disabled = true;
-			submitBtn.classList.remove("on");
+			btnFail()
 		} else if(userPassword.value !== userPasswordChkVal) { // 비밀번호 값이 일치 하지 않을 때
 			e.target.className = "fail";
 			e.target.nextElementSibling.textContent = "비밀번호 일치하지 않습니다.";
 			e.target.nextElementSibling.classList.add("on");
-			submitBtn.disabled = true;
-			submitBtn.classList.remove("on");
+			btnFail()
 		} else { // 비밀번호를 정상적으로 입력할 때
 			e.target.className = "good";
 			e.target.nextElementSibling.textContent = "";
@@ -138,8 +134,7 @@ if(userName && userPasswordChk && passwordBtnChk) {
 			submitBtn.disabled = false;
 			submitBtn.classList.add("on");
 		} else { // 이메일 비밀번호 둘다 조건에 맞지 않을 경우
-			submitBtn.disabled = true;
-			submitBtn.classList.remove("on");
+			btnFail()
 		}
 	}
 
@@ -156,8 +151,7 @@ if(userName && userPasswordChk && passwordBtnChk) {
 			submitBtn.disabled = false;
 			submitBtn.classList.add("on")
 		} else { // 이메일 비밀번호 둘다 조건에 맞지 않을 경우
-			submitBtn.disabled = true;
-			submitBtn.classList.remove("on");
+			btnFail()
 		}
 	}
 	
